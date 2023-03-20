988db0a626dd4100742263697c3285d6292a1918
Zwogli <Mathiaskohler@web.de>
2023-03-20 17:00:07 +0100

    cleanCode Java Script

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -24,28 +24,3 @@ function initMain(){
     renderMain("desserts");
     renderMain("drinks");
 }
-
-function renderMain(section){
-    let mainContainer = document.getElementById(`main_${section}`);
-    mainContainer.innerHTML = '';
-
-    for (let i = 0; i < restaurants[`${section}`].menu.length; i++) {
-        let Menu = restaurants[`${section}`].menu[i];
-        let Info = restaurants[`${section}`].info[i];
-        // let label = restaurants[`${section}`].label[i];
-        let Price = numberToString(restaurants[`${section}`].price[i]);
-
-        mainContainer.innerHTML += /*html*/`
-        <div class="main_section_card">
-            <div class="main_section_menu">
-                <h4 class="margin_b_8">${Menu}</h4>
-                <p class="t_14">${Info}</p>
-                <h4 class="margin_t_8">${Price}</h4>
-            </div>
-            <div class="main_section_icon">
-                <img class="icon_32 cicle_32" src="assets/img/icons/add.svg" alt="add">
-            </div>
-        </div>
-        `;
-    }
-}
--- assets/js/support.js
+++ assets/js/support.js
@@ -1,3 +1,17 @@
 function numberToString(price){
     return price.toLocaleString('de-DE', { maximumFractionDigits: 2, minimumFractionDigits: 2 }) + ' €';
 }
+
+function renderMain(section){
+    let mainContainer = document.getElementById(`main_${section}`);
+    mainContainer.innerHTML = '';
+
+    for (let i = 0; i < restaurants[`${section}`].menu.length; i++) {
+        let menu = restaurants[`${section}`].menu[i];
+        let info = restaurants[`${section}`].info[i];
+        // let label = restaurants[`${section}`].label[i];
+        let price = numberToString(restaurants[`${section}`].price[i]);
+
+        mainContainer.innerHTML += generateHtmlCards(menu, info, price);
+    }
+}
