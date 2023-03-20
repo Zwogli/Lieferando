5d2b0d60368496d24954bfaa3bfd17d4e36c0fdc
Zwogli <Mathiaskohler@web.de>
2023-03-20 11:08:35 +0100

    init JS main

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -1,14 +1,27 @@
 function render(){
     initHeader();
     initNavbar();
+    initMain();
 }
 
 function initHeader(){
-    generateHTMLHeader();
+    document.getElementById('h_logo').src = `${restaurants.logo}`;
+    document.getElementById('h_rest_name').innerHTML = `${restaurants.name}`;
 }
 
 function initNavbar(){
     let navbar = document.getElementById('navbar');
     navbar.innerHTML = '';
     navbar.innerHTML += generateHtmlNavbar();
+}
+
+function initMain(){
+    document.getElementById('main').innerHTML += generateHtmlMain();
+    generateImgsMain();
+    renderFavourit();
+    rednerSalad();
+    renderPizza();
+    renderHamburger();
+    renderDesserts();
+    renderDrinks();
 }
